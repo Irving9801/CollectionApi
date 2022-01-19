7746686fea838ada18b3379e9613e80dd396b6af
Irving Vasquez <ivasquez@hypernovalabs.com>
2022-01-19 00:04:46 -0500

    error

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken'
-
+console.log(process.env.JWT_SECRET,"SUI")
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: '30d',
