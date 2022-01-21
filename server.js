b66e53f9d93d77b685334a264ee18272b4e5ac8b
Irving Vasquez <ivasquez@hypernovalabs.com>
2022-01-20 23:24:12 -0500

    cors

--- server.js
+++ server.js
@@ -14,9 +14,12 @@ import bodyParser1 from "body-parser";
 import randomId from "random-id";
 import swaggerUi from "swagger-ui-express";
 // import swaggerDocument from "./swagger.json";
-import { createRequire } from 'module';
+import { createRequire } from "module";
+import cors from "cors";
+// const cors = require("cors");
+// var app = require('express')();
 const require = createRequire(import.meta.url);
-const swaggerDocument = require('./swagger.json');
+const swaggerDocument = require("./swagger.json");
 const customCss = fs1.readFileSync(process.cwd() + "/swagger.css", "utf8");
 dotenv.config();
 
@@ -26,7 +29,7 @@ const app = express(),
   bodyParser = bodyParser1,
   fs = fs1,
   port = 5000;
-
+app.use(cors());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -44,38 +47,18 @@ app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
-app.get("/api/config/paypal", (req, res) =>
-  res.send(process.env.PAYPAL_CLIENT_ID)
-);
-app.get('/', (req,res) => {
+app.get("/api/config/paypal", (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.send(process.env.PAYPAL_CLIENT_ID);
+});
+app.get("/", (req, res) => {
   res.send(`<h1>API Running on port ${port}</h1>`);
 });
-app.get('/api/cors', (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*');
-  res.send({ "msg": "This has CORS enabled 🎈" })
-  })
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-    console.log(`Server running in ${port} mode on port ${PORT}`.yellow.bold);
+app.get("/api/cors", (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.send({ msg: "This has CORS enabled 🎈" });
 });
-
-// const __dirname = path.resolve();
-// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
-// if (process.env.NODE_ENV === "production") {
-//   // app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "server"))
-//   );
-//   console.log("bebe")
-// }
-// //  else {
-//   console.log(process.env.NODE_ENV)
-//   app.get("/", (req, res) => {
-//     res.send(`<h1>API Running on port ${port}</h1>`);
-//   });
-// // }
-
-// app.use(notFound);
-// app.use(errorHandler);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running in ${port} mode on port ${PORT}`.yellow.bold);
+});
